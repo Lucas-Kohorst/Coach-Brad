17ca5859685972f4f0f4a211d5669b3e6fe32532
Lucas Kohorst <kohorstlucas@gmail.com>
2020-02-08 13:57:18 -0500

    Frontend init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Coach-Brad
 Brickhack VI
+
+# Tracking 
+1. Elbow Position
+2. Balance
+3. Leg Position
---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Upload from './components/Upload';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Coach Brad. </p>{" "}
+        <a
+          className="App-link"
+          href="https://github.com/Lucas-Kohorst/Coach-Brad"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>{" "}
+      </header>{" "}
+      <div>
+        <Upload />
+      </div>
+    </div>
+  );
+}
+
+export default App;
