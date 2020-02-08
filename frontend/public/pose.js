646a57923ad1d97a8663c15b6cf8833573a819d5
Lucas Kohorst <kohorstlucas@gmail.com>
2020-02-08 15:15:22 -0500

    Frontend Basic

---
+++ frontend/public/pose.js
@@ -0,0 +1,67 @@
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
+
+// the link to your model provided by Teachable Machine export panel
+const URL = "https://teachablemachine.withgoogle.com/models/_aMgfsAB/";
+let model, webcam, ctx, labelContainer, maxPredictions;
+
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    // load the model and metadata
+    // Refer to tmPose.loadFromFiles() in the API to support files from a file picker
+    model = await tmPose.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    // Convenience function to setup a webcam
+    const flip = false; // whether to flip the webcam
+    webcam = new tmPose.Webcam(500, 500, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    webcam.play();
+    window.requestAnimationFrame(loop);
+
+    // append/get elements to the DOM
+    const canvas = document.getElementById("canvas");
+    canvas.width = 500;
+    canvas.height = 500;
+    ctx = canvas.getContext("2d");
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) {
+        // and class labels
+        labelContainer.appendChild(document.createElement("div"));
+    }
+}
+
+async function loop(timestamp) {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+async function predict() {
+    // Prediction #1: run input through posenet
+    // estimatePose can take in an image, video or canvas html element
+    const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+    // Prediction 2: run input through teachable machine classification model
+    const prediction = await model.predict(posenetOutput);
+
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+        labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+
+    // finally draw the poses
+    drawPose(pose);
+}
+
+function drawPose(pose) {
+    ctx.drawImage(webcam.canvas, 0, 0);
+    // draw the keypoints and skeleton
+    if (pose) {
+        const minPartConfidence = 0.5;
+        tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+        tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+    }
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import Upload from './components/Upload';
+import Upload from "./components/Upload";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p> Coach Brad. </p>{" "}
+    <React.Fragment>
+      <div className="App App-header" id="wrapper">
+        <p> Coach Brad</p>{" "}
         <a
           className="App-link"
           href="https://github.com/Lucas-Kohorst/Coach-Brad"
@@ -15,11 +15,26 @@ function App() {
         >
           Github
         </a>{" "}
-      </header>{" "}
-      <div>
-        {/* <Upload /> */}
       </div>
-    </div>
+      <div
+        className="App App-header"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          backgroundColor: "#282c34"
+        }}
+        id="wrapper"
+      >
+        <div>
+          <canvas id="canvas"></canvas>
+        </div>
+        <div
+          id="label-container"
+          style={{ color: "white", paddingTop: "30vh" }}
+        ></div>
+      </div>
+    </React.Fragment>
   );
 }
 
